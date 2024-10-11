0c836f895e0a5f46656f3e3bda57b30628fbbe55
Molin Han <Molin.han@outlook.com>
2024-10-11 16:38:48 +0100

    start

---
+++ Mixed_Poisson.py
@@ -0,0 +1,28 @@
+import firedrake as fd
+import numpy as np
+import scipy as sp
+from matplotlib import pyplot as plt
+
+# Extruded Mesh
+m = fd.CircleManifoldMesh(20, radius=2)
+mesh = fd.ExtrudedMesh(m, 5, extrusion_type='radial')
+
+# Mixed Finite Element Space
+CG_1 = fd.FiniteElement("CG", fd.interval, 1)
+DG_0 = fd.FiniteElement("DG", fd.interval, 0)
+P1P0 = fd.TensorProductElement(CG_1, DG_0)
+RT_horiz = fd.HDivElement(P1P0)
+P0P1 = fd.TensorProductElement(DG_0, CG_1)
+RT_vert = fd.HDivElement(P0P1)
+RT = RT_horiz + RT_vert
+# FIXME: DG still need these operations?
+DG = fd.FunctionSpace(mesh, 'DG', 0)
+
+W = RT * DG
+
+# Test Functions
+sigma, u = fd.TrialFunction(W)
+tau, v = fd.TestFunctions(W)
+
+x, y = fd.SpatialCoordinate(mesh)
+
