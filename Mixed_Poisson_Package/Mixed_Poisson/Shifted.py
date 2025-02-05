67322199b99b4e15aab0069ae3c8c11774e52462
Molin Han <Molin.han@outlook.com>
2025-02-05 17:58:57 +0000

    working on shifted

---
+++ Mixed_Poisson_Package/Mixed_Poisson/Shifted.py
@@ -0,0 +1,177 @@
+from firedrake import *
+import numpy as np
+import scipy as sp
+from matplotlib import pyplot as plt
+from firedrake.output import VTKFile
+
+class ShiftedPoisson:
+    def __init__(self, height=pi/40, nlayers=20, horiz_num=80, radius=2, mesh="interval"):
+        '''
+        mesh : interval or circle to be extruded.
+        '''
+        self.height = height
+        self.rad = radius
+        self.ar = height/(2 * pi * radius)
+        self.dx = 2 * pi * radius / horiz_num
+        self.dz = height / nlayers
+        print(f"The aspect ratio is {self.ar}")
+        
+        # Extruded Mesh
+        if mesh == "interval":
+            self.m = UnitIntervalMesh(horiz_num, name='interval')
+            self.mesh = ExtrudedMesh(self.m, nlayers, layer_height = height/nlayers, extrusion_type='uniform')
+        if mesh == "circle":
+            self.m = CircleManifoldMesh(horiz_num, radius=radius, name='circle')
+            self.mesh = ExtrudedMesh(self.m, nlayers, layer_height = height/nlayers, extrusion_type='radial')
+
+        # Create a ExtrudedMesh Hierarchy to achieve the vertical lumping space
+        self.mh = MeshHierarchy(self.m, refinement_levels=0)
+        if mesh == "interval":
+                self.hierarchy = ExtrudedMeshHierarchy(self.mh, height,layers=[1, nlayers], extrusion_type='uniform')
+        if mesh == "circle":
+                self.hierarchy = ExtrudedMeshHierarchy(self.mh, height,layers=[1, nlayers], extrusion_type='radial')
+
+
+        # Mixed Finite Element Space
+        CG_1 = FiniteElement("CG", interval, 1)
+        DG_0 = FiniteElement("DG", interval, 0)
+        P1P0 = TensorProductElement(CG_1, DG_0)
+        RT_horiz = HDivElement(P1P0)
+        P0P1 = TensorProductElement(DG_0, CG_1)
+        RT_vert = HDivElement(P0P1)
+        RT_e = RT_horiz + RT_vert
+        RT = FunctionSpace(self.mesh, RT_e)
+        DG = FunctionSpace(self.mesh, 'DG', 0)
+        self.W = RT * DG
+
+        # Test Functions
+        self.u, self.p = TrialFunctions(self.W)
+        self.v, self.q = TestFunctions(self.W)
+
+        # Solution Functions
+        self.sol = Function(self.W) # solution in mixed space
+        self.u_sol, self.p_sol = split(self.sol)
+
+        self.x, self.y = SpatialCoordinate(self.mesh)
+
+    def build_f(self, option="regular"):
+        '''
+        option : regular or stiff or random
+        '''
+        DG = FunctionSpace(self.mesh, 'DG', 0)
+        theta = atan2(self.y,self.x)
+        if option == "regular":
+            self.f = Function(DG).interpolate(10 * exp(-pow(theta, 2)))
+
+        elif option == "stiff":
+            # self.f = Function(DG).interpolate(10 * exp(-pow(theta, 2) * (self.y -self.rad) / self.height))
+            self.f = Function(DG).interpolate(exp(-pow(theta, 2)*self.y))
+            #self.f = Function(DG).interpolate(exp(-pow(theta, 2)))
+
+        elif option == "random":
+            pcg = PCG64(seed=123456789)
+            rg = Generator(pcg)
+            self.f = rg.normal(DG, 1.0, 2.0)
+            # self.f = Function(DG).interpolate(10 * exp(-pow(theta, 2)))
+        else:
+            raise NotImplementedError("this option is not available. Please choose from regular, stiff or random.")
+        One = Function(DG).assign(1.0)
+        area = assemble(One*dx)
+        f_int = assemble(self.f*dx)
+        self.f.interpolate(self.f - f_int/area)
+
+    def build_direct_params(self):
+        self.params = {'ksp_type': 'preonly', 'pc_type':'lu', 'mat_type': 'aij', 'pc_factor_mat_solver_type': 'mumps'}
+
+    def build_ASM_MH_params(self):
+        self.params = {
+            'mat_type': 'matfree',
+            'ksp_type': 'gmres',
+            'snes_monitor': None,
+            # 'snes_type':'ksponly',
+            # 'ksp_monitor': None,
+            # "ksp_monitor_true_residual": None,
+            'pc_type': 'mg',
+            'pc_mg_type': 'full',
+            "ksp_converged_reason": None,
+            "snes_converged_reason": None,
+            'mg_levels': {
+                'ksp_type': 'richardson',
+                # "ksp_monitor_true_residual": None,
+                # "ksp_view": None,
+                "ksp_atol": 1e-50,
+                "ksp_rtol": 1e-10,
+                'ksp_max_it': 1,
+                'pc_type': 'python',
+                'pc_python_type': 'firedrake.AssembledPC',
+                'assembled_pc_type': 'python',
+                'assembled_pc_python_type': 'firedrake.ASMVankaPC',
+                'assembled_pc_vanka_construct_dim': 0,
+                'assembled_pc_vanka_sub_sub_pc_type': 'lu'
+                #'assembled_pc_vanka_sub_sub_pc_factor_mat_solver_type':'mumps'
+                },
+            'mg_coarse': {
+                'ksp_type': 'preonly',
+                'pc_type': 'lu'
+                }
+        }
+
+    def build_NonlinearVariationalSolver(self):
+        # Variational Problem
+        u = self.u_sol
+        p = self.p_sol
+        q = self.q
+        v = self.v
+        f = self.f
+        self.F = (inner(u, v) + div(v)*p + div(u)*q)*dx + f * q * dx
+
+        # Boundary conditions
+        bc1 = DirichletBC(self.W.sub(0), as_vector([0., 0.]), "top")
+        bc2 = DirichletBC(self.W.sub(0), as_vector([0., 0.]), "bottom")
+        bc3 = DirichletBC(self.W.sub(0), as_vector([0., 0.]), "on_boundary")
+        self.bcs = [bc1, bc2, bc3]
+
+        v_basis = VectorSpaceBasis(constant=True) #pressure field nullspace
+        self.nullspace = MixedVectorSpaceBasis(self.W, [self.W.sub(0), v_basis])
+        trans_null = VectorSpaceBasis(constant=True)
+        self.trans_nullspace = MixedVectorSpaceBasis(self.W, [self.W.sub(0), trans_null])
+
+        self.prob_w = NonlinearVariationalProblem(self.F, self.sol, bcs=self.bcs)
+        self.solver_w = NonlinearVariationalSolver(self.prob_w,
+                                                    nullspace=self.nullspace,
+                                                    transpose_nullspace=self.trans_nullspace,
+                                                    solver_parameters=self.params, 
+                                                    options_prefix='mixed_nonlinear')
+
+    def solve(self, monitor=False):
+        self.solver_w.solve()
+        if monitor:
+            self.sol_final = self.solver_w.snes.ksp.getSolution().getArray()
+            np.savetxt(f'sol_final.out',self.sol_final)
+
+    def write(self):
+        sol_u, sol_p = self.sol.subfunctions
+        sol_file = VTKFile('sol_MH.pvd')
+        sol_file.write(sol_u, sol_p)
+
+
+
+if __name__ == "__main__":
+        horiz_num = 80
+        height = pi / 20
+        nlayers = 20
+        radius = 2
+        mesh = "interval"
+        option = "regular"
+
+        equ = ShiftedPoisson(height=height, nlayers=nlayers, horiz_num=horiz_num, radius=radius, mesh=mesh)
+        print(f"The calculation is down in a {equ.m.name} mesh.")
+        equ.build_f(option=option)
+        equ.build_ASM_MH_params()
+        # equ.params_direct()
+        # equ.build_LinearVariationalSolver()
+        equ.build_NonlinearVariationalSolver()
+        equ.solve()
+        print("!!!!!!!!!!!!!!!",norm(assemble(equ.F, bcs=equ.bcs).riesz_representation()))
+        equ.write()
+
